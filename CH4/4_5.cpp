14c2d2d11034897089cf990bd7afdda98e3a3cec
Juan Carlos Chavez <lolzthisismyemail@gmail.com>
2015-09-28 13:20:54 -0700

    4_5.cpp Was Started.
    Started question 5.

---
+++ CH4/4_5.cpp
@@ -0,0 +1,14 @@
+/* Coder: Juan Chavez
+ * Date:  9.25.2015
+ * Objective: Implement a function to check if a binary tree is also a 
+ * binary search tree.
+ */
+
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
